1f2dd7224a75945a5f336f3557ac87b5a499fcd6
kouta-wolf <koutaookami18@gmail.com>
2026-04-24 09:07:59 +0900

    test: #14 ログイン関連のテスト作成

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -4,19 +4,15 @@ RSpec.describe "Sessions", type: :system do
   describe "正常系" do
     let(:user) { create(:user) }
 
-    it '正しい情報であればログイン後トップページに遷移すること' do
-      visit login_path
+    it '正しい情報であればログイン後gameページに遷移すること' do
+      visit new_session_path
 
       fill_in "メールアドレス", with: user.email
       fill_in "パスワード", with: user.password
-      click_button "ログイン"
+      click_button "ログインする"
 
       expect(page).to have_content "ログインしました"
-      expect(current_path).to eq root_path
+      expect(current_path).to eq games_index_path
     end
-  end
-
-  describe '異常系' do
-
   end
 end
