0813c92735b7400da1f6186333673984deba6459
kouta-wolf <koutaookami18@gmail.com>
2026-04-24 14:28:19 +0900

    fix: ローカルテスト周りを修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,34 +1,13 @@
-require 'socket'
+require 'capybara/rspec'
+require 'selenium-webdriver'
 
-Capybara.register_driver :remote_chrome do |app|
-  if ENV['CI'].present?
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-  else
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1680,1050')
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :remote,
-      url: ENV.fetch('SELENIUM_DRIVER_URL', 'http://selenium:4444/wd/hub'),
-      capabilities: options
-    )
-  end
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
 end
 
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :remote_chrome
-
-    if ENV['CI'].blank?
-      Capybara.server_host = '0.0.0.0'
-      Capybara.app_host = "http://#{IPSocket.getaddress(Socket.gethostname)}:3000"
-    end
-  end
-end
+Capybara.javascript_driver = :selenium_chrome_headless
