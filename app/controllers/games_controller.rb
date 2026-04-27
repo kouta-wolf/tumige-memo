40ddf11d6e03a69ce081400cc83d7c2251bdfa78
kouta-wolf <koutaookami18@gmail.com>
2026-04-27 20:41:45 +0900

    feat,fix: #58 sessionにrouteを変更したため変更した部分とログアウト機能実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,5 @@ class GamesController < ApplicationController
   def show
     @user = current_user
     @user_games = @user.user_games.includes(:game)
-    # @reviews = @user.reviews.order(created_at: :desc).limit(5)
   end
 end
