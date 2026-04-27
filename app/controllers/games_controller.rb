71b87482bd60c74e6825d40410513cc37e18559d
kouta-wolf <koutaookami18@gmail.com>
2026-04-27 16:19:17 +0900

    feat: #55 マイページの表示に必要そうなコントローラーを整備

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,10 @@
 class GamesController < ApplicationController
   def index
   end
+
+  def show
+    @user = current_user
+    @user_games = @user.user_games.includes(:game)
+    # @reviews = @user.reviews.order(created_at: :desc).limit(5)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[ new create ]
   resources :sessions, only: %i[ new create destroy]
+  resource :game, only: %i[ show ]
 end
